d0d31a843b1b83bd723d62b7cd2838c448cd40e0
Mohamed Ansardeen <ansar.rnd@gmail.com>
2023-12-15 09:18:03 +0800

    working app

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module org.openjfx {
     requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires com.google.gson;
+    opens org.openjfx to javafx.fxml, javafx.graphics;
     exports org.openjfx;
 }
